85a7b50b7016d11e45a022e2807d47f9db8a5e0c
Xuan Bui <xuanb1910180@student.ctu.edu.vn>
2022-09-30 16:21:28 +0700

    Tao trang them lien he moi

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,46 @@
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+
+export default {
+    components: {
+        ContactForm,
+    },
+    data() {
+        return {
+            contact: {
+                name: "",
+                email: "",
+                address: "",
+                phone: "",
+                favorite: false
+            },
+            message: "",
+        };
+    },
+    methods: {
+        async addContact(data) {
+            try {
+                await ContactService.create(data);
+                this.message = "Liên hệ được thêm mới thành công.";
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+
+    created() {
+        // this.getContact(this.id);
+        this.message = "";
+    },
+};
+
+</script>
+
+<template>
+    <div v-if="contact" class="page">
+        <h4>Thêm Liên hệ</h4>
+        <ContactForm :contact="contact" @submit:contact="addContact" />
+        <p>{{ message }}</p>
+    </div>
+</template>
